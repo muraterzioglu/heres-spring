15c58660e8a04a807ebad7e9835e1797737b1e72
Murat Terzioglu <its.muraterzioglu@gmail.com>
2023-06-24 02:05:50 +0300

    🔧 Simple ApplicationProperties Usage

--- src/main/java/codes/murat/heresspring/Application.java
+++ src/main/java/codes/murat/heresspring/Application.java
@@ -1,5 +1,6 @@
 package codes.murat.heresspring;
 
+import codes.murat.heresspring.Configs.SimpleConfigs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +13,17 @@ import java.util.Map;
 @SpringBootApplication
 public class Application {
 	private final Map<String, String> homeHashMap = new HashMap<String, String>();
+	private final SimpleConfigs configs;
+	public Application(SimpleConfigs configs) {
+		this.configs = configs;
+	}
 
 	@RequestMapping("/")
 	Map<String, String> home() {
 		homeHashMap.put("code", "200");
 		homeHashMap.put("message", "Hello World!");
+		homeHashMap.put("author", configs.author);
+		homeHashMap.put("propFileDefaultValue", configs.lastname);
 		return homeHashMap;
 	}
 
---
+++ src/main/java/codes/murat/heresspring/Configs/SimpleConfigs.java
@@ -0,0 +1,13 @@
+package codes.murat.heresspring.Configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SimpleConfigs {
+    @Value("${app.author}")
+    public String author;
+
+    @Value("${app.lastname:default_value}")
+    public String lastname;
+}
